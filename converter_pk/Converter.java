5f80d4f19a823aa951146e3c2966ca9d9dbb855e
Angelo Carlotto <angelocarlotto@gmail.com>
2024-05-29 09:44:37 -0400

    first commit

---
+++ converter_pk/Converter.java
@@ -0,0 +1,42 @@
+package converter_pk;
+
+public class Converter {
+
+    private static Converter converter;
+
+    public Converter() {
+        converter = new Converter();
+    }
+
+    public static float ConvertKGToLP(float kilogramsValue) {
+        return converter.getPoundValue(kilogramsValue);
+    }
+
+    public static float ConvertLPToKG(float poundsValue) {
+        return 0;
+    }
+
+    public static float ConvertCLToFAH(float celsiusValue) {
+        return 0;
+    }
+
+    public static float ConvertFAHToCL(float fahrenheitValue) {
+        return 0;
+    }
+
+    private float getKilogramsValue(float poundsValue) {
+        return 0;
+    }
+
+    private float getPoundValue(float kilogramsValue) {
+        return 0;
+    }
+
+    private float getFahenheitValue(float celciusValue) {
+        return 0;
+    }
+
+    private float getCelciusValue(float fahenheitValue) {
+        return 0;
+    }
+}
