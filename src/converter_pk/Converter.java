1efb02ae72dba42322ed5a965afdd78fd59d5b90
Angelo Carlotto <angelocarlotto@gmail.com>
2024-05-30 09:19:27 -0400

    meet assignment expecifications

--- src/converter_pk/Converter.java
+++ src/converter_pk/Converter.java
@@ -2,37 +2,79 @@ package src.converter_pk;
 
 public class Converter {
 
+    /**
+     * 
+     * @param value
+     * @param option 1 will return Fahenheit, 2 will return Celsius
+     * @return
+     */
+    public static double getTemperature(double value, int option) {
+        double tempResult = 0;
+        switch (option) {
+            case 1:
+                tempResult = getFahenheitValue(value);
+                break;
+            case 2:
+                tempResult = getCelciusValue(value);
+                break;
+            default:
+                System.err.println("Invalid Choice! Only 1 or 2 is allowed");
+        }
 
-
-    public static float ConvertKGToLP(float kilogramsValue) {
-        return getPoundValue(kilogramsValue);
+        return tempResult;
     }
 
-    public static float ConvertLPToKG(float poundsValue) {
+    /**
+     * 
+     * @param value
+     * @param option 1 will return Kilograms, 2 will return Pounds
+     * @return
+     */
+    public static double getMass(double value, int option) {
+        double massResult = 0;
+        switch (option) {
+            case 1:
+                massResult = getKilogramsValue(value);
+                break;
+            case 2:
+                massResult = getPoundValue(value);
+                break;
+            default:
+                System.err.println("Invalid Choice! Only 1 or 2 is allowed");
+        }
+        return massResult;
+    }
+
+    /*
+     * public static double ConvertKGToLP(double kilogramsValue) {
+     * return getPoundValue(kilogramsValue);
+     * }
+     * 
+     * public static double ConvertLPToKG(double poundsValue) {
+     * return 0;
+     * }
+     * 
+     * public static double ConvertCLToFAH(double celsiusValue) {
+     * return 0;
+     * }
+     * 
+     * public static double ConvertFAHToCL(double fahrenheitValue) {
+     * return 0;
+     * }
+     */
+    private static double getKilogramsValue(double poundsValue) {
         return 0;
     }
 
-    public static float ConvertCLToFAH(float celsiusValue) {
+    private static double getPoundValue(double kilogramsValue) {
+        return kilogramsValue * 2.2046226218;
+    }
+
+    private static double getFahenheitValue(double celciusValue) {
         return 0;
     }
 
-    public static float ConvertFAHToCL(float fahrenheitValue) {
-        return 0;
-    }
-
-    private static float getKilogramsValue(float poundsValue) {
-        return 0;
-    }
-
-    private static float getPoundValue(float kilogramsValue) {
-        return (float) (kilogramsValue * 2.2046226218);
-    }
-
-    private static float getFahenheitValue(float celciusValue) {
-        return 0;
-    }
-
-    private static float getCelciusValue(float fahenheitValue) {
+    private static double getCelciusValue(double fahenheitValue) {
         return 0;
     }
 }
--- src/converter_pk/TestConverter.java
+++ src/converter_pk/TestConverter.java
@@ -2,9 +2,9 @@ package src.converter_pk;
 
 public class TestConverter {
     public static void main(String[] args) {
-        System.out.println("Celsius to Fahereiht:"+Converter.ConvertCLToFAH(10));
-        System.out.println("Fahereits to Celsius:"+Converter.ConvertFAHToCL(10));
-        System.out.println("Kilograms to Pounds:"+Converter.ConvertKGToLP(10));
-        System.out.println("Pounds to Kilograms:"+Converter.ConvertLPToKG(10));
+        System.out.println("Celsius to Fahereiht:"+Converter.getTemperature(10,1));
+        System.out.println("Fahereits to Celsius:"+Converter.getTemperature(10,2));
+        System.out.println("Kilograms to Pounds:"+Converter.getMass(10,2));
+        System.out.println("Pounds to Kilograms:"+Converter.getMass(10,1));
     }
 }
